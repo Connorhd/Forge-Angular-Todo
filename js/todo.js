5320f61841a75a89684f087a511fc81968c7fc50
Connor Dunn <connor@trigger.io>
2012-08-29 11:39:33 +0100

    Use prefs to persist todo list.

--- js/todo.js
+++ js/todo.js
@@ -1,11 +1,23 @@
 function TodoCtrl($scope) {
-	$scope.todos = [
-		{text:'learn angular', done:true},
-		{text:'build an angular app', done:false}];
+	// Grab persisted todo list
+	forge.prefs.get("todos", function (todos) {
+		// Updating Angular model, $apply makes sure the view is updated too.
+		$scope.$apply(function () {
+			if (todos) {
+				$scope.todos = todos;
+			} else {
+				$scope.todos = [
+					{text:'learn angular', done:true},
+					{text:'build an angular app', done:false},
+					{text:'extend angular app to work with Forge', done:false}];
+			}
+		});
+	});
  
 	$scope.addTodo = function() {
 		$scope.todos.push({text:$scope.todoText, done:false});
 		$scope.todoText = '';
+		forge.prefs.set("todos", $scope.todos);
 	};
  
 	$scope.remaining = function() {
@@ -22,6 +34,7 @@ function TodoCtrl($scope) {
 		angular.forEach(oldTodos, function(todo) {
 			if (!todo.done) $scope.todos.push(todo);
 		});
+		forge.prefs.set("todos", $scope.todos);
 	};
 }
 
